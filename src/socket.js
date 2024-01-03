900246f5be3444f89f54998c0d9fc226a2ea4a21
vishvp.streamspace@gmail.com <vishvp.streamspace@gmail.com>
2024-01-03 17:52:12 +0530

    change socket url

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:4567';
+const URL = process.env.REACT_APP_BACKEND_;
 
 export const socket = io(URL,{
     autoConnect:false
